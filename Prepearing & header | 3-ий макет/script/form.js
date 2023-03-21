f625a6487270224e907afbca9be14459400c6874
dmytro <tsekhandmytro@gmail.com>
2023-03-21 17:41:19 +0200

    FORM

---
+++ Prepearing & header | 3-ий макет/script/form.js
@@ -0,0 +1,8 @@
+const signOpen = document.querySelector('.form-wrap');
+const signX = document.querySelector('.X');
+signOpen.addEventListener('click',function () {
+    form-wrap.classList.add('active')
+})
+signX.addEventListener('click',function () {
+    form-wrap.classList.add('notActiv')
+})
